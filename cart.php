f818e66758cfdce6efd43f36c1daa3084af5b946
Darshankumar Madihalli <43309529+DarshanMadihalli99@users.noreply.github.com>
2020-07-08 15:30:35 +0530

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,29 @@
+<?php
+require 'includes/common.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cart | E-Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" >
+        <script type="text/javascript" src="bootstrap/js/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <link rel='stylesheet' type='text/css' href="style.css">
+    </head>
+    <body>
+        <?php
+        include 'includes/header.php';
+        ?>
+        <div class="container">
+            <div class="row">
+                
+            </div>
+        </div>
+        <?php
+        require './includes/footer.php';
+        ?>  
+    
+    </body>
+</html>
